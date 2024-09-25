39e7ac11d1eee5d16c3ccc448d584d126af15678
Aimee Kilmartin <aimeekilmartin@gmail.com>
2024-09-25 12:27:42 +1200

    create api for pokemon details

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,9 +1,14 @@
 import request from 'superagent'
-import { PokemonList } from '../models/pokemon'
+import { Pokemon, PokemonList } from '../models/pokemon'
 
 const url = 'https://pokeapi.co/api/v2/'
 
 export async function fetchPokemonList(): Promise<PokemonList[]> {
   const res = await request.get(`${url}/pokemon/?limit=151`)
   return res.body.results
+}
+
+export async function fetchPokemon(name: string): Promise<Pokemon> {
+  const res = await request.get(`${url}/pokemon/${name}`)
+  return res.body
 }
